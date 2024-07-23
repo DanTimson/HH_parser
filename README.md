4b857383b946efed992f8cd12e9b124e9646a250
Kitsu <krasilnikovkolya4654@gmail.com>
2024-07-23 16:59:39 +0300

    quick start in README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# HH_parser
+
+## Quick start
+```commandline
+git clone https://github.com/Kitsumetri/HH_parser.git
+cd HH_parser
+py -m venv .venv
+.venv\scripts\activate
+pip3 install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from src.utils import setup_logger
 
 
 def main():
-    setup_logger()
-    print('Main start')
+    setup_logger(stdout_log=False)
     logging.info('Start pool')
+    logging.info('Start')
 
 
 if __name__ == '__main__':
